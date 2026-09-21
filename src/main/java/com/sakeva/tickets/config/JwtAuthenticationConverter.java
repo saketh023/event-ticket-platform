be945b7e36b82c10d80edc70693a02ab8ee5871b
Saketh Vadlamudi <v.saketh23@gmail.com>
2026-09-21 19:15:15 -0400

    Added custom JWT converter to extract user roles, added the JwtConverter in SecurityConfig and locked events endpoints to organizer

---
+++ src/main/java/com/sakeva/tickets/config/JwtAuthenticationConverter.java
@@ -0,0 +1,43 @@
+package com.sakeva.tickets.config;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Component
+public class JwtAuthenticationConverter implements Converter<Jwt, JwtAuthenticationToken> {
+
+    @Override
+    public JwtAuthenticationToken convert(Jwt jwt) {
+        Collection<GrantedAuthority> authorities = extractAuthorities(jwt);
+        return new JwtAuthenticationToken(jwt, authorities);
+    }
+
+    // * method to extract the roles assigned on Keycloak to each user from the JWT to be used in the application
+    // * it is represented using a SimpleGrantedAuthority (which is a repr. of specific permission, privilege, or role
+    // granted to an authenticated user)
+    private Collection<GrantedAuthority> extractAuthorities(Jwt jwt) {
+        Map<String, Object> realmAccess = jwt.getClaim("realm_access");
+
+        if (null == realmAccess || !realmAccess.containsKey("roles")) {
+            return Collections.emptyList();
+        }
+
+        @SuppressWarnings("unchecked")
+        List<String> roles = (List<String>) realmAccess.get("roles");
+
+        return roles.stream()
+                .filter(role -> role.startsWith("ROLE_"))
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+}
--- src/main/java/com/sakeva/tickets/config/SecurityConfig.java
+++ src/main/java/com/sakeva/tickets/config/SecurityConfig.java
@@ -14,16 +14,22 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http, UserProvisioningFilter userProvisioningFilter)
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http,
+            UserProvisioningFilter userProvisioningFilter,
+            JwtAuthenticationConverter jwtAuthenticationConverter)
             throws Exception {
         http.authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.GET, "/api/v1/published-events/**")
                         .permitAll()
+                        .requestMatchers("/api/v1/events")
+                        .hasRole("ORGANIZER")
                         .anyRequest()
                         .authenticated())
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .oauth2ResourceServer(
+                        oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter)))
                 .addFilterAfter(userProvisioningFilter, BearerTokenAuthenticationFilter.class);
         return http.build();
     }
